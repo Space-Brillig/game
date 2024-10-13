889c5790df92506e3046ad9a157699bdcfbd710a
Raphael Vicente <rafaelx9.oliveira@gmail.com>
2024-10-12 21:39:16 -0300

    feat: menu buttons, screens exchange, alien catching mechanism, spaceship lifebar, meteors and regenerative items, define ach screen function, spawning mechanism, backgrounds and organization of files

---
+++ characters.py
@@ -0,0 +1,127 @@
+import pygame
+
+#define colours
+red = (255, 0, 0)
+green = (0, 255, 0)
+
+#Spaceship class
+class Spaceship(pygame.sprite.Sprite):
+    def __init__(self, x, y, health):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('src/sprites/spaceship/spaceship.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.health_start = health
+        self.health_remaining = health
+        self.last_shot = pygame.time.get_ticks()
+    
+    #set movement on the spaceship
+    def update(self, SURFACE_HEIGHT, SURFACE_WIDTH, SURFACE):
+        if self.rect.bottom < SURFACE_HEIGHT - 35:
+            self.rect.y += 8
+		#set movement speed
+        speed = 8
+		#set a cooldown variable
+        cooldown = 500 #milliseconds
+         
+		#key press and movement
+        key = pygame.key.get_pressed()
+        if (key[pygame.K_w] or key[pygame.K_UP]) and self.rect.top > 10:
+            self.rect.y -= speed + 8
+        if (key[pygame.K_a] or key[pygame.K_LEFT]) and self.rect.left > 10:
+            self.rect.x -= speed
+        if (key[pygame.K_s] or key[pygame.K_DOWN]) and self.rect.bottom < SURFACE_HEIGHT - 10:
+            self.rect.y += speed
+        if (key[pygame.K_d] or key[pygame.K_RIGHT]) and self.rect.right < SURFACE_WIDTH - 10:
+            self.rect.x += speed
+        
+        #CLT shooting mechanism
+        time_now = pygame.time.get_ticks()
+        if key[pygame.K_SPACE] and time_now - self.last_shot > cooldown:
+            clt = CLT(self.rect.centerx, self.rect.top)
+            clt_group.add(clt)
+            self.last_shot = time_now
+
+        #update mask
+        self.mask = pygame.mask.from_surface(self.image)
+        
+        #draw health bar
+        pygame.draw.rect(SURFACE, red, (self.rect.x, (self.rect.bottom + 10), self.rect.width, 15))
+        if self.health_remaining > 0:
+            pygame.draw.rect(SURFACE, green, (self.rect.x, (self.rect.bottom + 10), int(self.rect.width * (self.health_remaining / self.health_start)), 15))
+        else:
+            self.kill()
+        
+#CLT class
+class CLT(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("src/sprites/clt/clt.png")
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.center = [x, y]
+
+    def update(self):
+        self.rect.y -= 10
+        if self.rect.bottom < 0 or pygame.sprite.spritecollide(self, meteor_group, True, pygame.sprite.collide_mask):
+            self.caught = True #catch by collision:
+            self.kill()
+
+#Alien class
+class Alien(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("src/sprites/alien/shrek.jpeg")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.caught = False
+    
+    def update(self, SURFACE_WIDTH, SURFACE_HEIGHT):
+        self.rect.y += 4
+        if pygame.sprite.spritecollide(self, clt_group, True, pygame.sprite.collide_mask):
+            self.caught = True #catch by collision
+
+        #drag to the closest side if it's caught
+        if self.caught:
+            if self.rect.x >= SURFACE_WIDTH // 2:
+                self.rect.x += 4
+            else:
+                self.rect.x -= 4
+            self.rect.y += 4
+
+        if self.rect.top > SURFACE_HEIGHT or self.rect.left > SURFACE_WIDTH or self.rect.right < 0:
+            self.kill()
+
+#Meteor class
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale, image, isharmful):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(f'src/sprites/meteor/{image}.png')
+        self.isharmful = isharmful
+        self.height = self.image.get_height()
+        self.width = self.image.get_width()
+        self.image = pygame.transform.scale(self.image, (int(self.width * scale), int(self.height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.mask = pygame.mask.from_surface(self.image)
+
+    def update(self, SURFACE_HEIGHT, spaceship):
+        self.rect.y += 10
+        if self.rect.top > SURFACE_HEIGHT:
+            self.kill()
+        if pygame.sprite.spritecollide(self, spaceship_group, False, pygame.sprite.collide_mask):
+            self.kill()
+
+            #if it's the regenerative item or not
+            if self.isharmful:
+                spaceship.health_remaining -= 1
+            elif spaceship.health_remaining > 0 and spaceship.health_remaining < spaceship.health_start:
+                spaceship.health_remaining += 1
+
+
+#game sprite groups
+spaceship_group = pygame.sprite.Group()
+clt_group = pygame.sprite.Group()
+alien_group = pygame.sprite.Group()
+meteor_group = pygame.sprite.Group()
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import buttons
+import characters
 import math
 from random import randint
 pygame.init()
@@ -14,132 +16,181 @@ SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Brillig: The Extraordinary Jobs Galaxy!")
 
-#define background
-scroll = 0
-bg = pygame.image.load("bg.png").convert()
-bg_height = bg.get_width()
+#load title screen buttons' images
+continue_button_img = pygame.image.load('src/sprites/buttons/continue.jpeg').convert_alpha()
+new_game_button_img = pygame.image.load('src/sprites/buttons/new_game.jpeg').convert_alpha()
 
-def draw_bg():
-    screen.blit(bg, (0,0))
+#create title screen buttons' instances
+continue_button = buttons.Button(110, 100, continue_button_img, 0.8)
+new_game_button = buttons.Button(100, 700, new_game_button_img, 0.8)
 
-#define alien spawning event
-ALIEN_SPAWN_EVENT = pygame.USEREVENT + 1
-pygame.time.set_timer(ALIEN_SPAWN_EVENT, 2500)  # Spawn every 200 milliseconds
+#load Phase Menu buttons' images
+phase_img = pygame.image.load('src/sprites/buttons/continue.jpeg').convert_alpha()
 
-#Spaceship class
-class Spaceship(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('spaceship.png')
-        self.rect = self.image.get_rect()
-        self.rect.center = [x, y]
-        self.mask = pygame.mask.from_surface(self.image)
-        self.last_shot = pygame.time.get_ticks()
-    
-    #set movement on the spaceship
-    def update(self):
-        if self.rect.bottom < SCREEN_HEIGHT - 10:
-            self.rect.y += 3
-		#set movement speed
-        speed = 8
-		#set a cooldown variable
-        cooldown = 500 #milliseconds
-         
-		#key press and movement
-        key = pygame.key.get_pressed()
-        if (key[pygame.K_w] or key[pygame.K_UP]) and self.rect.top > 10:
-            self.rect.y -= speed + 3
-        if (key[pygame.K_a] or key[pygame.K_LEFT]) and self.rect.left > 10:
-            self.rect.x -= speed
-        if (key[pygame.K_s] or key[pygame.K_DOWN]) and self.rect.bottom < SCREEN_HEIGHT - 10:
-            self.rect.y += speed
-        if (key[pygame.K_d] or key[pygame.K_RIGHT]) and self.rect.right < SCREEN_WIDTH - 10:
-            self.rect.x += speed
-        
-        #CLT shooting mechanism
-        time_now = pygame.time.get_ticks()
-        if key[pygame.K_SPACE] and time_now - self.last_shot > cooldown:
-            clt = CLT(self.rect.centerx, self.rect.top)
-            clt_group.add(clt)
-            self.last_shot = time_now
-        
-#CLT class
-class CLT(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("clt.png")
-        self.rect = self.image.get_rect()
-        self.rect.center = [x, y]
-        self.mask = pygame.mask.from_surface(self.image)
+#create Phase Menu buttons' instances
+phase1_button = buttons.Button(100, 300, phase_img, 0.5)
+phase2_button = buttons.Button(100, 400, phase_img, 0.5)
+phase3_button = buttons.Button(100, 500, phase_img, 0.5)
 
-    def update(self):
-        self.rect.y -= 10
-        if self.rect.bottom < 0:
-            self.kill()
+#define creature spawning event
+CREATURES_SPAWN_EVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(CREATURES_SPAWN_EVENT, 1500)  # Spawn every 1500 milliseconds
 
-#Alien class
-class Alien(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("shrek.jpeg")
-        self.rect = self.image.get_rect()
-        self.rect.center = [x, y]
-        self.mask = pygame.mask.from_surface(self.image)
-        self.last_creation = pygame.time.get_ticks()
-    
-    def update(self):
-        self.rect.y += 4
-        if pygame.sprite.spritecollide(self, clt_group, False, pygame.sprite.collide_mask):
-            self.kill()
+#creature spawning function
+def spawn_creatures(a_prop, m_prop, r_prop): #spawning based on proportions, respectively: alien, meteor and regenerative item
 
-#alien spawning function
-def create_aliens():
-    alien = Alien(randint(10, SCREEN_WIDTH - 20), randint(-100, -50))
-    alien_group.add(alien)
-
-#game sprite groups
-spaceship_group = pygame.sprite.Group()
-clt_group = pygame.sprite.Group()
-alien_group = pygame.sprite.Group()
+    props = [a_prop, m_prop, r_prop]
+    props.sort()
+    random = randint(1, props[2])
+    for item in props:
+        if item == a_prop and random <= item:
+            alien = characters.Alien(randint(30, SCREEN_WIDTH - 30), randint(-100, -50))
+            characters.alien_group.add(alien)
+            break
+        elif item == m_prop and random <= item:
+            meteor = characters.Meteor(randint(10, SCREEN_WIDTH - 10), randint(-100, -50), 0.05, "meteor", True)
+            characters.meteor_group.add(meteor)
+            break
+        elif item == r_prop and random <= item:
+            meteor = characters.Meteor(randint(10, SCREEN_WIDTH - 10), randint(-100, -50), 0.7, "r_item", False)
+            characters.meteor_group.add(meteor)
+            break
 
 #create spaceship
-spaceship = Spaceship(int(SCREEN_WIDTH / 2), SCREEN_HEIGHT - 100)
-spaceship_group.add(spaceship) 
+spaceship = characters.Spaceship(int(SCREEN_WIDTH / 2), SCREEN_HEIGHT - 100, 5)
+characters.spaceship_group.add(spaceship)
 
-run = True
-while run:
+#draw background
+def draw_bg(bg):
+     
+    screen.blit(bg, (0,0))
 
-    clock.tick(fps)
+#event handlers
+def event_handlers():
 
-    draw_bg()
-    
-    #scrolling background
-    for i in range(0, math.ceil(SCREEN_HEIGHT  / bg_height) + 1):
-        screen.blit(bg, (0, - (i * bg_height + scroll)))
-        spaceship_group.draw(screen)
-    scroll -= 5
-    if abs(scroll) > bg_height:
-        scroll = 0
-
-    #update spaceship
-    spaceship.update()
-
-    #update space groups
-    clt_group.update()
-    alien_group.update()
-
-    #draw sprite groups
-    spaceship_group.draw(screen)
-    clt_group.draw(screen)
-    alien_group.draw(screen)
-    
-    #event handlers
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #quit game
-            run = False
-        elif event.type == ALIEN_SPAWN_EVENT: #spawn alien
-            create_aliens()
+            return True
 
-    pygame.display.update()
+#Title Screen function
+def title_screen():
+
+    clock.tick(fps - 30)
+
+    #setting the background
+    bg = pygame.image.load("src/sprites/background/space.png").convert()
+    bg = pygame.transform.scale(bg, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    i = 0
+
+    run = True
+    while run:
+        
+        draw_bg(bg)
+        
+        # blackhole animation
+        i += 1
+        if i == 50:
+            i = 1
+        blackhole = pygame.image.load(f"src/sprites/background/blackhole/frame-{i}.gif").convert()
+        blackhole = pygame.transform.scale(blackhole, (int(blackhole.get_width() * 2), int(blackhole.get_height() * 2)))
+        screen.blit(blackhole, (SCREEN_WIDTH // 2 - 200, SCREEN_HEIGHT // 2 - 100))
+
+        #actions depending on clicked buttons
+        if continue_button.draw(screen):
+            return (0) #calls the phase_menu function
+        if new_game_button.draw(screen):
+            return (1) #clear global variables
+        
+        if event_handlers():
+            run = False
+
+        pygame.display.update()
+
+#Phase Menu function
+def phase_menu():
     
-pygame.quit()
+    run = True
+    while run:
+
+        screen.fill((100, 100, 240))
+
+        #actions depending on clicked buttons
+        if phase1_button.draw(screen):
+            return (0) #calls the phase function with phase 1 parameters
+        if phase2_button.draw(screen):
+            return (1) #calls the phase function with phase 2 parameters
+        if phase3_button.draw(screen):
+            return (2) #calls the phase function with phase 3 parameters
+        
+        if event_handlers():
+            run = False
+
+        pygame.display.update()
+
+#Phase function
+def phase(background):
+
+    #define background
+    bg = pygame.image.load(background).convert()
+    bg = pygame.transform.scale(bg, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    bg_height = bg.get_height()
+    scroll = 0
+
+    run = True
+    while run:
+
+        clock.tick(fps)
+
+        draw_bg(bg)
+        
+        #scrolling background
+        for i in range(0, math.ceil(SCREEN_HEIGHT  / bg_height) + 1):
+            screen.blit(bg, (0, - (i * bg_height + scroll)))
+        scroll -= 5
+        if abs(scroll) > bg_height:
+            scroll = 0
+
+        #update spaceship
+        spaceship.update(SCREEN_HEIGHT, SCREEN_WIDTH, screen)
+
+        #update space groups
+        characters.clt_group.update()
+        characters.alien_group.update(SCREEN_WIDTH, SCREEN_HEIGHT)
+        characters.meteor_group.update(SCREEN_HEIGHT, spaceship)
+
+        #draw sprite groups
+        characters.spaceship_group.draw(screen)
+        characters.clt_group.draw(screen)
+        characters.alien_group.draw(screen)
+        characters.meteor_group.draw(screen)
+        
+        #event handlers
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: #quit game
+                run = False
+            elif event.type == CREATURES_SPAWN_EVENT: #spawn creatures
+                spawn_creatures(5, 7, 10)
+
+        pygame.display.update()
+
+#Main game logic
+def main():
+
+    title_result = title_screen()
+
+    if title_result == 0:
+
+        phase_result = phase_menu()
+        if phase_result == 0:
+            phase("src/sprites/background/bg.png") #phase 1
+        elif phase_result == 1:
+            phase("src/sprites/background/space.png") #phase 2
+        elif phase_result == 2:
+            phase("src/sprites/background/bg.png") #phase 3
+
+    elif title_result == 1:
+        phase() 
+    else:
+        pygame.quit()
+
+main()
